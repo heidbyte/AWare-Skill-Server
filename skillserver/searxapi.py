d0427dabb0b92c54644f33a3f47dac94d23e0828
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-03-30 15:47:59 +0200

    Update searxapi.py

--- skillserver/searxapi.py
+++ skillserver/searxapi.py
@@ -6,14 +6,8 @@ def callAPI(question,lang = "en"):
 	try:
 		with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json") as url:
 			datas = json.loads(url.read().decode())
-			for url in range(10):
-				if("youtube.com" in datas["results"][url]["url"] == False):
-					return datas["results"][url]["url"]
 			return datas["results"][0]["url"]
 	except:
 		with urllib.request.urlopen("https://searx.decatec.de/search?q=:" + lang + "%20" + question + "&format=json") as url:
 			datas = json.loads(url.read().decode())
-			for url in range(10):
-				if("youtube.com" in datas["results"][url]["url"] == False):
-					return datas["results"][url]["url"]
 			return datas["results"][0]["url"]
