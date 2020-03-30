f607ac91acb3847160b816c621a56669474f1d3c
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-03-30 11:23:00 +0200

    fix searxapi

--- skillserver/searxapi.py
+++ skillserver/searxapi.py
@@ -2,16 +2,16 @@ import urllib.request, json
 import urllib.parse
 
 def callAPI(question,lang = "en"):
-  question = urllib.parse.quote(question)
-  try:
-    with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json") as url:
-      datas = json.loads(url.read().decode())
-      for url in range(10):
-			  if("youtube.com" in datas["results"][url]["url"] == False):
-				  return datas["results"][url]["url"]
-  except:
-    with urllib.request.urlopen("https://searx.decatec.de/search?q=:" + lang + "%20" + question + "&format=json") as url:
-      datas = json.loads(url.read().decode())
-      for url in range(10):
-			  if("youtube.com" in datas["results"][url]["url"] == False):
-				  return datas["results"][url]["url"]
+	question = urllib.parse.quote(question)
+	try:
+		with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json") as url:
+			datas = json.loads(url.read().decode())
+			for url in range(10):
+				if("youtube.com" in datas["results"][url]["url"] == False):
+					return datas["results"][url]["url"]
+	except:
+		with urllib.request.urlopen("https://searx.decatec.de/search?q=:" + lang + "%20" + question + "&format=json") as url:
+			datas = json.loads(url.read().decode())
+			for url in range(10):
+				if("youtube.com" in datas["results"][url]["url"] == False):
+					return datas["results"][url]["url"]
