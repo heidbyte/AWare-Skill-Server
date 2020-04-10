e7ca17ec22d703e62b0470556fc67570c4e7f138
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-04-10 18:24:38 +0200

    change to roberta

--- skillserver/predictTransformer.py
+++ skillserver/predictTransformer.py
@@ -14,7 +14,7 @@ myfile.close()
 numKeys = len(keys)-1
 
 					
-model = MultiLabelClassificationModel('distilbert', 'de_transformer', num_labels=numKeys, use_cuda = True, args={'reprocess_input_data': True, 'overwrite_output_dir': True, 'num_train_epochs': 15, "train_batch_size": 16, "eval_batch_size": 16, 'no_cache': True, 'use_cached_eval_features' : False, 'save_model_every_epoch':False})
+model = MultiLabelClassificationModel('roberta', 'de_transformer', num_labels=numKeys, use_cuda = True, args={'reprocess_input_data': True, 'overwrite_output_dir': True, 'num_train_epochs': 15, "train_batch_size": 16, "eval_batch_size": 16, 'no_cache': True, 'use_cached_eval_features' : False, 'save_model_every_epoch':False})
 
 
 while 1:
