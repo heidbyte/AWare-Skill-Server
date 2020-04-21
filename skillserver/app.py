caab804aa9f5febe7dececddd79dc181c00a5edf
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-04-21 11:47:45 +0200

    set host to local

--- skillserver/app.py
+++ skillserver/app.py
@@ -546,4 +546,4 @@ def foo():
 # run server on given port
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
-	app.run(host='0.0.0.0', port=args["port"], debug=True)
+	app.run(host='127.0.0.1', port=args["port"], debug=True)
