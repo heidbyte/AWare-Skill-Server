453c84102408b4477589e6f0dbef4a70dcb11d32
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-04-22 16:18:27 +0200

    Update searxapi.py

--- skillserver/searxapi.py
+++ skillserver/searxapi.py
@@ -1,14 +1,16 @@
 import urllib.request, json
 import urllib.parse
+import random
 
 def callAPI(question,lang = "en"):
+	searxs = ["https://searx.decatec.de/","https://searx.be/","https://searx.my.id/","https://search.snopyta.org/","https://searx.hlfh.space/"]
 	question = urllib.parse.quote(question)
 	try:
 		with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json") as url:
 			datas = json.loads(url.read().decode())
 			return datas["results"][0]["url"]
 	except:
-		with urllib.request.urlopen("https://searx.be/search?q=:" + lang + "%20" + question + "&format=json") as url:
+		with urllib.request.urlopen(random.choice(searxs) + "search?q=:" + lang + "%20" + question + "&format=json") as url:
 			datas = json.loads(url.read().decode())
 			try:
 				return datas["results"][0]["url"]
