b3e0f046e1fcafbcadcb125cad08fbf26822f53d
flozi00 <47894090+flozi00@users.noreply.github.com>
2020-04-26 14:23:05 +0200

    limit search engines

--- skillserver/searxapi.py
+++ skillserver/searxapi.py
@@ -4,9 +4,10 @@ import random
 
 def callAPI(question,lang = "en"):
 	searxs = ["https://searx.decatec.de/","https://searx.be/","https://searx.my.id/","https://search.snopyta.org/","https://searx.hlfh.space/"]
+	engines = ["google","yahoo","duckduckgo","yandex"]
 	question = urllib.parse.quote(question)
 	try:
-		with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json") as url:
+		with urllib.request.urlopen("http://127.0.0.1:8888/search?q=:" + lang + "%20" + question + "&format=json&engines=" + random.choice(engines)) as url:
 			datas = json.loads(url.read().decode())
 			return datas["results"][0]["url"]
 	except:
